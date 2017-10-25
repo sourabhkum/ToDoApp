ad4fd6a6d938be819084142f5e72749207161c66
sourabh <sourabhcybo@gmail.com>
2017-10-25 09:52:09 +0530

    set to deploy application

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
-var MONGODB_URI='mongodb://<kumar>:<kumar7852>@ds127065.mlab.com:27065/todos'
+var MONGODB_URI='mongodb://kumar:kumar7852@ds127065.mlab.com:27065/todos'
 mongoose.connect(MONGODB_URI,{ useMongoClient: true });
 
 module.exports={mongoose}
