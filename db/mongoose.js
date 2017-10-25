5c9517c1a9bdb041c813080d0400f4dad8fc8a40
sourabh <sourabhcybo@gmail.com>
2017-10-25 09:32:56 +0530

    set to deploy application

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
-MONGODB_URI='mongodb://<sourabhcybo>:<Sourabh@7852>@ds127065.mlab.com:27065/todos'
+MONGODB_URI='mongodb://<kumar>:<Kumar@123>@ds127065.mlab.com:27065/todos'
 mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp',{ useMongoClient: true });
 
 module.exports={mongoose}
